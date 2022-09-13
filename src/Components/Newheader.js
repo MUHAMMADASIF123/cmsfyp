d6a64ec94080188387a8ccff35c98d185422d14c
MUHAMMADASIF123 <52148492+MUHAMMADASIF123@users.noreply.github.com>
2022-09-13 16:17:22 +0500

    some chages before pull

--- src/Components/Newheader.js
+++ src/Components/Newheader.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
-import './Header.css';
+import './header.css';
 const Newheader = () => {
   const history = useNavigate();
   const logout = () => {
