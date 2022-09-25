f291b63d5ee7f59daab130d0f9d6456707634dec
alisher61 <alisher4461@gmail.com>
2022-09-25 15:18:04 +0500

    no addition

--- src/Components/AdminSide/Admin.js
+++ src/Components/AdminSide/Admin.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 function Admin() {
   return (
+    //no addition
     <div className='container mt-5 mb-5 justify-content-center '>
       <form className='w-100 mb-5 '>
         <div className='row mt-5 shadow justify-content-start d-flex bg-white'>
